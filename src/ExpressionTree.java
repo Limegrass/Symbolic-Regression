846145579e36f8cafdbcfe8c3d7980d626dd375c
Limegrass <jani@davidson.edu>
2017-02-09 21:21:12 -0500

    DataSet Comments
    Commented DataSet, waiting on Expression Tree class from Chris

---
+++ src/ExpressionTree.java
@@ -0,0 +1,19 @@
+
+/**
+ * Probably already done by Chris, to be imported
+ * @author James
+ *
+ */
+public class ExpressionTree 
+{
+	
+	private ExpressionNode root;
+	
+	private class ExpressionNode
+	{
+		private int operation;
+		private int value;
+	}
+	
+	
+}
