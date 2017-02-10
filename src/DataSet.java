75f5d7caa3d31509e3da8fff631a35fb0f072bd2
Limegrass <jani@davidson.edu>
2017-02-09 21:21:38 -0500

    Commented DataSet
    Commenting, JavaDocs.

--- src/DataSet.java
+++ src/DataSet.java
@@ -4,30 +4,60 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Set;
 
-public class DataSet {
+
+/**
+ * Parse CSV Files into a Data Set type for use
+ * as a expression fitness function evaluator 
+ * when using genetic programming for symbolic regress
+ * @author James Ni
+ *
+ */
+public class DataSet 
+{
 	HashMap<Double, Double> data;
 	
-	public static void main(String[] args){
+	/**
+	 * Main function, as a tester
+	 * @param args Arguments
+	 */
+	public static void main(String[] args)
+	{
 		String fileName = "C:/Eclipse Java Workspace/Symbolic Regression/src/dataset1.csv";
 		DataSet dataSet = new DataSet(fileName);
 	}
 
-	public DataSet(String fileName){
-		data = new HashMap<Double, Double>();
-		BufferedReader reader = null;
-		String line = "";
-		String separator = ",";
+	
+	/**
+	 * Data set constructor using a CSV file
+	 * Assumed two columns of data, an input column and
+	 * an output column.
+	 * @param fileName The name of the file to convert into a data set.
+	 */
+	public DataSet(String fileName)
+	{
+		data = new HashMap<Double, Double>();	//A map is a logical way to hold all the values
+		//An alternative would be a hash map if I knew the range of the data.
+		// For the data sets we seem to be working with, there is two decimals of precision, but
+		// a map will be more robust.
+		BufferedReader reader = null;	//Reader object 
+		String line = "";				//String for line by line reading
+		String separator = ",";			//Changeable parameter if the CSV is delimited by something else
+		
 		try{
+			//Initialize the BufferedReader
 			reader = new BufferedReader(new FileReader(fileName));
-			while((line = reader.readLine())!= null){
+			//Read all lines and input them into the map
+			while((line = reader.readLine())!= null)
+			{
 				String[] values = line.split(separator);
-				try{
+				try
+				{
 					double x = Double.parseDouble(values[0]);
-					double y = Double.parseDouble(values[0]);
+					double y = Double.parseDouble(values[1]);
 					data.put(x, y);
-
 				}
-				catch(NumberFormatException err){
+				catch(NumberFormatException err)
+				{
 					//err.printStackTrace();
 				}
 			}
@@ -36,22 +66,33 @@ public class DataSet {
 			//					System.out.println(x.get(i) + " " + y.get(i));
 			//				}
 		} 
-		catch(IOException err){
+		catch(IOException err)
+		{
 			err.printStackTrace();
 		}
 		finally{
-			if(reader != null){
-				try{
+			if(reader != null)
+			{
+				try
+				{
 					reader.close();	
 				}
-				catch(IOException err){
+				catch(IOException err)
+				{
 					err.printStackTrace();
 				}
 			}
 		}
 	}
 
-	public double fitness(String expression){
+	/**
+	 * Fitness function to complete for checking the
+	 * fitness of a linear regression
+	 * @param expression Expression to evaluate how well it fits the data.
+	 * @return The fitness of the inputed expression tree relative to the data set.
+	 */
+	public double fitness(ExpressionTree expression)
+	{
 		double fitness = 0;
 		//Parse the expression to be a function
 		Set<Double> keySet = data.keySet();
@@ -63,7 +104,8 @@ public class DataSet {
 			//Square the difference
 			//Add it to total fitness
 			keysIterated++;
-			if((double)keysIterated/dataPoints > .5){
+			if((double)keysIterated/dataPoints > .5)
+			{
 				break;
 			}
 		}
